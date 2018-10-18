56857f6de3717c82e19c93d4d933f7a24196c4ba
Arif Hidayat <afht.dc.xxx@gmail.com>
2018-10-19 00:57:09 +0700

    Update: Add Main Controller

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Arif Hidayat
+ * on October 19, 2018
+ */
+
+namespace App;
+
+
+class MainController extends Controller
+{
+    public function index(){
+        return "Index";
+    }
+}
