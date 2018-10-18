ee6552283982e09d33feadfc9dde808e7ad5507c
Arif Hidayat <afht.dc.xxx@gmail.com>
2018-10-19 01:00:37 +0700

    Update: Add index in public directory

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Arif Hidayat
+ * on October 18, 2018
+ */
+
+namespace App;
+
+use LianaCore\Controller\BaseController;
+
+class Controller extends BaseController
+{
+    public function __construct()
+    {
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Arif Hidayat
+ * on October 18, 2018
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Load autoloader from composer.
+|
+*/
+require __DIR__.'/../vendor/autoload.php';
